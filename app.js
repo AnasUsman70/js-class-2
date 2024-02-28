6fc907ac2c3d4d80bac1fda435eef242cfd41806
AnasUsman70 <anasusman598@gmail.com>
2024-02-28 12:16:09 +0500

    js class 2

---
+++ app.js
@@ -0,0 +1,69 @@
+//                                    JS ASSIGNMENT 2 
+// CHAPTER 4
+// Q1
+// var Name , anas , text;
+// Q2
+//                                    legal variables
+// var _legalVariable
+// var $hello
+// var Legal234
+// var specialcharacter
+// var lost_world
+//                                    illegal variables
+// var 1Point
+// var sea%view
+// var !Legal
+// var _olx*
+// var hello/world
+// Q3
+document.write("<b><h2>Rules for naming JS variables</h2></b>")
+document.write("Variable names can only contain numbers. $. Letters or _. For example <b>$my_1stVariable</b> <br>")
+document.write("Variables must begin with a Letters. _ or $. For example <b>$name, _name</b> or <b>name</b> <br>")
+document.write("Variable names are case sensitive<br>")
+document.write("Variable names should not be JS Keywords<br>")
+// chapter5
+// Q1
+var a = 3
+var b = a + 5
+document.write("Sum is 3 and 5 is "+b+"<br>")
+// Q2
+// SUBTRACTION
+var B = a - 5
+document.write("Subtraction is 3 and 5 is "+B+"<br>")
+// MULTIPLICATION
+var c = a * 5
+document.write("multipilication is 3 and 5 is "+c+"<br>")
+// DIVISION
+var d = a / 5
+document.write("division is 3 and 5 is "+d+"<br>")
+// MODULUS
+var d = a % 5
+document.write("modulus is 3 and 5 is "+d+"<br>")
+// Q3
+var no;
+document.write("Value after variable declaration is " + no +"<br>")
+var no = 5
+document.write("initial value: " + no++ +"<br>")
+document .write("value after increment is: " + no +"<br>")
+document.write ("value after addition is: " + (no + 7) +"<br>")
+var no = 13
+document.write ("value after decrement is: " + --no +"<br>")
+var no = 12
+document.write("the remainder is: " + no % 3 / 3 + "<br>")
+// Q4
+var ticket = 600
+document.write( "Total cost to buy 5 ticket to a movie is "+ 600*5 + "PKR<br>")
+// Q5
+var table = 9
+document.write( "Table of 9 <br>" +
+    table + " x 1 = " + table * 1 + "<br>" +
+    table + " x 1 = " + table * 2 + "<br>" +
+    table + " x 1 = " + table * 3 + "<br>" +
+    table + " x 1 = " + table * 4 + "<br>" +
+    table + " x 1 = " + table * 5 + "<br>" +
+    table + " x 1 = " + table * 6 + "<br>" +
+    table + " x 1 = " + table * 7 + "<br>" +
+    table + " x 1 = " + table * 8 + "<br>" +
+    table + " x 1 = " + table * 9 + "<br>" +
+    table + " x 1 = " + table * 10 + "<br>" 
+)
